be46e1ab95b6400762909353a7f1c6a8e463c175
Weiming Zhai <wmzhai@gmail.com>
2015-03-20 10:05:21 +0800

    实际使用错误报告

---
+++ client/helpers/errors.js
@@ -0,0 +1,12 @@
+// 本地（仅客户端）集合
+Errors = new Mongo.Collection(null);
+
+throwError = function(message) {
+  Errors.insert({message: message});
+};
+
+Template.errors.helpers({
+  errors: function() {
+    return Errors.find();
+  }
+});
