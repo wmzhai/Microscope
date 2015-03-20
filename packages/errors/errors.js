e26869b7df88fb897ac5d81d1a5926542db7b5e0
Weiming Zhai <wmzhai@gmail.com>
2015-03-20 16:17:25 +0800

    Display comments on postPage

--- packages/errors/errors.js
+++
@@ -1,9 +0,0 @@
-// Write your package code here!
-Errors = {
-  // Local (client-only) collection
-  collection: new Mongo.Collection(null),
-
-  throw: function(message) {
-    Errors.collection.insert({message: message, seen: false})
-  }
-};
